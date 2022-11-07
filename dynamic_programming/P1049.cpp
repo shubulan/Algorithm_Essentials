d5458b106082b20289f53a5285618ca1b82e7ddc
shubulan <gaoyulong@xiaomi.com>
2022-11-07 12:19:15 +0800

    title: 补充P1049第二种解法
    Signed-off-by: shubulan <gaoyulong@xiaomi.com>

--- dynamic_programming/P1049.cpp
+++ dynamic_programming/P1049.cpp
@@ -8,17 +8,29 @@ int main() {
     dp[0] = 1;
     cin >> V >> n;
     int x;
+    //for (int i = 1; i <= n; i++) {
+    //    cin >> x;
+    //    for (int j = V; j >= x; j--) {
+    //        if (dp[j - x]) dp[j] = dp[j - x];
+    //    }
+    //}
+    //for (int i = V; i >= 0; i--) 
+    //    if (dp[i] != 0) {
+    //        cout << V - i << endl;
+    //        break;
+    //    }
+    dp[0] = 0;
+    for (int i = 1; i <= V; i++) {
+        dp[i] = i;
+    }
     for (int i = 1; i <= n; i++) {
         cin >> x;
         for (int j = V; j >= x; j--) {
-            if (dp[j - x]) dp[j] = dp[j - x];
+            if (dp[j - x] < dp[j]) dp[j] = dp[j - x];
         }
     }
-    for (int i = V; i >= 0; i--) 
-        if (dp[i] != 0) {
-            cout << V - i << endl;
-            break;
-        }
+    cout << dp[V] << endl;
+
 
     return 0;
 }
