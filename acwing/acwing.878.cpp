fd91bddf553bd11c8bc0f8c00acd5898f4b1b706
shubulan <gaoyulong@xiaomi.com>
2023-12-06 12:44:27 +0800

    chinese residents theory

--- acwing/acwing.878.cpp
+++ acwing/acwing.878.cpp
@@ -1,41 +1,40 @@
-#include <algorithm>
-#include <cassert>
-#include <cmath>
-#include <cstring>
 #include <iostream>
-#include <map>
-#include <queue>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <utility>
-#include <vector>
+#include <cstring>
+#include <algorithm>
 
 using namespace std;
 typedef long long LL;
-typedef pair<int, int> PII;
-
-const int N = 100005;
-
-int exgcd(int a, int b, int &x, int &y) {
-    if (!b) {
-        x = 1, y = 0;
-        return a;
-    }
-    int r = exgcd(b, a % b, y, x);
-    y -= (a / b) * x;
-    return r;
+LL exgcd(LL a, LL b, LL &x, LL& y) {
+  if (!b) {
+    x = 1, y = 0;
+    return a;
+  }
+  int d = exgcd(b, a % b, y, x);
+  y -= (a / b)*x;
+  return d;
 }
-
 int main() {
   int n;
+  LL a1, m1;
   cin >> n;
+  n--;
+  cin >> m1 >> a1;
   while (n--) {
-    int a, b, m, x, y;
-    cin >> a >> b >> m;
-    int r = exgcd(a, m, x, y);
-    if (b % r) cout << "impossible" << endl;
-    else cout << (LL)x * b / r % m << endl;
+    LL a2, m2;
+    cin >> m2 >> a2;
+    LL k1, k2;
+    int d = exgcd(m1, m2, k1, k2);
+    if ((a2 - a1) % d) {
+      cout << -1;
+      return 0;
+    }
+    k1 *= (a2 - a1) / d;
+    LL t = m2 / d;
+    k1 = (k1 % t + t) % t;
+    a1 = k1 * m1 + a1;
+    m1 = abs(m1 * t);
   }
+  cout << (a1 % m1 + m1) % m1 << endl;
+  
   return 0;
 }
